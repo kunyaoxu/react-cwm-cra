a28eae419ad87b16822ffd46299801173e7ec9b8
SDxBacon <z141134553@gmail.com>
2021-04-17 17:12:07 +0800

    refactor: 💡 加個useMobile hook

---
+++ src/constants/media-query.js
@@ -0,0 +1,10 @@
+const MAP_MEDIA_QUERY = {
+  huge: '1440px',
+  large: '1170px',
+  medium: '768px',
+  small: '450px',
+};
+
+export const MEDIA_THRESHOLD = 'large';
+
+export const MEDIA_THRESHOLD_FOR_HOOK = `(min-width: ${MAP_MEDIA_QUERY[MEDIA_THRESHOLD]}px)`;
