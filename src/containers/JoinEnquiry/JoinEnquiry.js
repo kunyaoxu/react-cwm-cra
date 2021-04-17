05ae34a834e26ed7cfbad16d491dc9011738ac4f
kunyao.xu <a663321765@gmail.com>
2021-04-17 16:57:51 +0800

    feat: 🎸 form 外部 layout

--- src/containers/JoinEnquiry/JoinEnquiry.js
+++ src/containers/JoinEnquiry/JoinEnquiry.js
@@ -1,7 +1,68 @@
-import { Wrapper } from './styled';
+import { Form, Input, Button, Select, Row } from 'antd';
+import qs from 'qs';
+import { Wrapper, AntdFrom, AntdFromItem } from './styled';
+
+const API_URL =
+  'https://script.google.com/macros/s/AKfycbxxQ2nlf-8v24S775ZUMGhKDv9vb5tnNg-CGMQjLOtVVI-RZn7cp0s2EyloFxzO6eR-uQ/exec';
+
+const { Option } = Select;
+
+const layout = {
+  labelCol: { span: 24 },
+  wrapperCol: { span: 24 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
 
 const JoinEnquiry = () => {
-  return <Wrapper id="join-enquiry">Join Enquiry</Wrapper>;
+  const [form] = Form.useForm();
+
+  const onFinish = (data) => {
+    console.log(data);
+
+    fetch(
+      `${API_URL}?${qs.stringify(
+        { fields: data.fields.map((val) => val ?? null) },
+        {
+          arrayFormat: 'repeat',
+        }
+      )}`
+    )
+      .then(() => {
+        console.log('fields', data?.fields, 'succes');
+      })
+      .catch(() => console.error('google sheet error!'));
+  };
+
+  return (
+    <Wrapper id="join-enquiry">
+      <AntdFrom form={form} name="control-hooks" onFinish={onFinish}>
+        <AntdFromItem {...layout} label="Gender" name={['fields', 0]}>
+          <Input placeholder="請輸入" type="text" />
+        </AntdFromItem>
+        <AntdFromItem {...layout} label="Gender" name={['fields', 1]}>
+          <Select placeholder="請選擇">
+            <Option value="male">male</Option>
+            <Option value="female">female</Option>
+            <Option value="other">other</Option>
+          </Select>
+        </AntdFromItem>
+        <AntdFromItem {...layout} label="Gender" name={['fields', 2]}>
+          <Select placeholder="2請選擇">
+            <Option value="male">male</Option>
+            <Option value="female">female</Option>
+            <Option value="other">other</Option>
+          </Select>
+        </AntdFromItem>
+        <AntdFromItem>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </AntdFromItem>
+      </AntdFrom>
+    </Wrapper>
+  );
 };
 
 export default JoinEnquiry;
--- src/containers/JoinEnquiry/styled.js
+++ src/containers/JoinEnquiry/styled.js
@@ -1,7 +1,35 @@
+import { Form, Input, Button, Select, Row } from 'antd';
 import styled from 'styled-components';
+import media from 'styled-media-query';
+import Flex from 'common/Flex';
 
-export const Wrapper = styled.div`
+export const Wrapper = styled(Flex)`
+  flex-direction: column;
+  align-items: center;
   width: 100%;
-  height: 2502px;
   background: #566c6c;
+  font-family: 'Noto Sans TC';
+`;
+
+export const AntdFrom = styled(Form)`
+  background-color: #768e91;
+  width: 100%;
+  max-width: 90%;
+  padding: 6% 7.7%;
+
+  ${media.greaterThan('large')`
+    max-width: 76%;
+    padding: 4% 10.5%;
+  `}
+`;
+
+export const AntdFromItem = styled(Form.Item)`
+  background-color: #768e91;
+  margin-bottom: 12px;
+
+  ${media.greaterThan('large')`
+    margin-bottom: 20px;
+    padding: 0 15px;
+    margin-bottom: 20px;
+  `}
 `;
